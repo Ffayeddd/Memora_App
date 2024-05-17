4ab10f9b958cef43b67f73c8b4ee514444a5e897
fatma <fatmaafayedd199@gmail.com>
2024-05-17 04:03:50 +0300

    Initial commit

--- lib/features/mainScreen/presentation/widget/usedAppBar.dart
+++ lib/features/mainScreen/presentation/widget/usedAppBar.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class UsedAppBar extends StatelessWidget implements PreferredSizeWidget
-{  UsedAppBar({super.key,  required this.title});
+class UsedAppBar extends StatelessWidget implements PreferredSizeWidget {
+  UsedAppBar({super.key, required this.title});
   final String title;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        title: Text(title,style: TextStyle(
-        color: Colors.white, // Set the text color
-        fontSize: 30,),),
-      centerTitle: true,
+      flexibleSpace: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white, // Set the text color
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ),
     );
+  }
 
-}
-@override
-Size get preferredSize => Size.fromHeight(150);
+  @override
+  Size get preferredSize => Size.fromHeight(150.h);
 }
